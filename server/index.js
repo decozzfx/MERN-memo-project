f88d9b4c909e09eb7b0768e54018d36f2c9d69a2
decozzfx <m.f.r.ozil@gmail.com>
2022-02-17 21:35:16 +0700

    CRUD application back&front end

--- server/index.js
+++ server/index.js
@@ -10,11 +10,12 @@ dotenv.config()
 
 const app = express()
 
-app.use('/posts', postRoutes)
 
 app.use(bodyParser.json({ limit : '30mb', extended : true }))
 app.use(bodyParser.urlencoded({ limit : '30mb', extended : true }))
 app.use(cors())
+
+app.use('/posts', postRoutes)
 
 const PORT = process.env.PORT || 5000
 
